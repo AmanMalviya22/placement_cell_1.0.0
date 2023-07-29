bc0aa11f0722635878eafd3ea7d77d7e422b0d7d
Aman Malviya <amanaman26802@gmail.com>
2023-07-29 10:15:38 +0530

    create folders for routes controllers config models views uploads

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const express=require('express');
-const port=process.env.PORT || 8000;
-const app=express();
-
-app.listen(port,function(err){
-    if(err){
-        console.log('err in listening');
-        return;
-    }
-    console.log('server is listening on port ',port);
-})
-
-
+const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const port = process.env.PORT || 8000;
+const app = express();
+const db = require("./config/mongoose");
+app.listen(port, function (err) {
+  if (err) {
+    console.log("err in listening");
+    return;
+  }
+  console.log("server is listening on port ", port);
+});
